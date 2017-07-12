003c10b8574fda05ecd5702759512c5203fe98fa
Mahasak Pijittum <mahasak@gmail.com>
2017-07-12 23:12:34 +0700

    remove unused code and spacing

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-
 	"github.com/mrtomyum/paybox_device/ctrl"
 )
 
 func main() {
 	r := gin.Default()
-	//r.LoadHTMLGlob("view/**/*")
-	//r.Static("/public", "./view/public")
-	//r.Static("/css", "./view/public/css")
-	//r.Static("/js", "./view/public/js")
 	app := ctrl.Router(r)
 	app.Run(":9999")
 }
